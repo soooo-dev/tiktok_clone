0ca56d6760d5ab92f04f979fcf848f93987310a2
SBS <bumsoo64@naver.com>
2023-08-13 22:02:34 +0900

    23/08/13 Authentication 1

---
+++ lib/features/authentication/widgets/auth_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class AuthButton extends StatelessWidget {
+  final String text;
+  final FaIcon icon;
+  final Widget widget;
+
+  const AuthButton({
+    super.key,
+    required this.text,
+    required this.icon,
+    required this.widget,
+  });
+
+  void _onAuthButtonTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => widget,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _onAuthButtonTap(context),
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Container(
+          padding: const EdgeInsets.all(
+            Sizes.size14,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey.shade300,
+              width: Sizes.size1,
+            ),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: icon,
+              ),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
