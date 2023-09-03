16d647b4fb4a77e601bdf2e9c272d6b076eeb8bf
SBS <bumsoo64@naver.com>
2023-09-03 17:10:38 +0900

    23/09/03 Onboarding 1

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -19,6 +20,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const InterestsScreen(),
+        ));
       }
     }
   }
@@ -43,6 +47,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                     hintText: "Emal",
                   ),
                   validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "Please write your email";
+                    }
                     return null;
                   },
                   onSaved: (newValue) {
@@ -57,6 +64,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                     hintText: "Password",
                   ),
                   validator: (value) {
+                    if (value != null && value.isEmpty) {
+                      return "Please write your password";
+                    }
                     return null;
                   },
                   onSaved: (newValue) {
